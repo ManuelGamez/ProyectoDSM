c1b3ae20d7b90758e887e5bfe0f02aa159bfefd9
Benja02 <eleazaralfaro41@gmail.com>
2021-04-19 16:34:09 -0600

    diseño de historial de compra

---
+++ app/src/main/java/com/example/proyectodsm/Historial.java
@@ -0,0 +1,14 @@
+package com.example.proyectodsm;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class Historial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historial);
+    }
+}
